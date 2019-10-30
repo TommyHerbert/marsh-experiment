0edf7e206a67d77b830ba7e4fe36e95e94ba27b2
Tommy Herbert <tommyherbert@gmail.com>
2019-10-30 12:20:34 +0000

    Initialise the database with models

---
+++ app/models.py
@@ -0,0 +1,29 @@
+from app import db
+
+
+class OwnerInterest(db.Model):
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'), primary_key=True)
+    interest_id = db.Column(db.Integer, db.ForeignKey('interest.id'), primary_key=True)
+    active = db.Column(db.Boolean)
+    interest = db.relationship('Interest', back_populates='owners')
+    owner = db.relationship('Owner', back_populates='interests')
+
+
+class Owner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    mobile = db.Column(db.String)
+    interests = relationship('OwnerInterest', back_populates='owner')
+
+    def __repr__(self):
+        return '<Owner {}>'.format(self.name)
+
+
+class Interest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    owners = relationship('OwnerInterest', back_poulates='interest')
+
+    def __repr__(self):
+        return '<Owner {}>'.format(self.name)
+
